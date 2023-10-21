ae432ff4d5132d7eb0d54dfb6a0442a689e2af57
Anne Auf dem Brinke <a.aufdembrinke@eos-ts.com>
2023-10-21 21:11:59 +0200

    save current status

--- README.md
+++ README.md
@@ -9,3 +9,21 @@
 
 - `pre-commit install`
 - Create a virtual environment and install `requirements.txt`
+
+## Analysis
+
+- The first (unnamed) column is not an id since there are duplicates but I also could not see a pattern in the data indicating its meaning.
+- For some columns the decimal separator was unclear. Under normal circumstances I would talk to someone on the team sourcing the data to figure out what was wrong with the data collection.
+- Distribution of integer columns:
+  - Feature 36: Very split distribution, either 0 or ~600.
+  - Feature 54: Nearly all 0 (~95%).
+  - Feature 57: Nearly all 0 (~95%).
+  - Feature 61: Nearly all 0 (~94%).
+  - Feature 62: Many values ~600 (~73%).
+- Distribution of float columns:
+  - Feature 13, 14 & 15: Nearly all ~400.
+  - Feature 20: Nearly all close to 0.
+  - Feature 23, 24 & 25: Very split distribution, either below 100 or ~360.
+  - Feature 28: Nearly all close to 50.
+- There seems to be some error happening in the data collection regularly on December 31st (only one data entry) and around end of March (138 instead of 144 data entries).
+- For 42 Timestamps there are two data points. There should be some logic to decide which one to keep.
---
+++ analysis.py
@@ -0,0 +1,107 @@
+"""Case study."""
+import pandas as pd
+
+INT_COLUMNS = [
+    "Nacelle Position (avg)",
+    "Feature 0",
+    "Feature 34",
+    "Feature 36",
+    "Feature 54",
+    "Feature 57",
+    "Feature 61",
+    "Feature 62",
+]
+
+FLOAT_COLUMNS = ["Feature 26"]
+
+INCONSISTENT_COLUMNS = [
+    "Wind Speed (avg)",
+    "Active Power (avg)",
+]
+
+
+def drop_rows(df: pd.DataFrame) -> pd.DataFrame:
+    """Drops rows that may skew data analysis.
+
+    Args:
+        df: pandas DataFrame
+
+    Returns:
+        pandas DataFrame
+    """
+    # days with too few data entries
+    date_count = df["Datum (Anlage)"].value_counts(sort=False).reset_index()
+    date_list = date_count[date_count["count"] == 1]["Datum (Anlage)"]
+
+    new_df = df[~df["Datum (Anlage)"].isin(date_list)]
+
+    # duplicate timestamps
+    new_df = new_df.drop_duplicates(subset=["Zeit (Anlage)"]).copy()
+
+    return new_df
+
+
+def transform_column_types(df: pd.DataFrame) -> None:
+    """Transforms the column types for columns which were not classified correctly.
+
+    Changes to the dataframe are happening inplace.
+
+    Args:
+        df: pandas DataFrame
+    """
+    # datetime
+    df["Zeit (Anlage)"] = pd.to_datetime(
+        df["Datum (Anlage)"] + df["Zeit (Anlage)"], format="%d.%m.%y%H:%M:%S"
+    )
+    df["Datum (Anlage)"] = pd.to_datetime(df["Datum (Anlage)"], format="%d.%m.%y")
+
+    for column in INCONSISTENT_COLUMNS:
+        df[column] = pd.to_numeric(df[column], errors="coerce")
+
+    for column in INT_COLUMNS:
+        try:
+            df[column] = df[column].astype("Int64")
+        except ValueError:
+            df[column] = df[column].astype(float)
+            df[column] = df[column].astype("Int64")
+
+    for column in FLOAT_COLUMNS:
+        df[column] = df[column].astype(float)
+
+
+def drop_columns(df: pd.DataFrame) -> None:
+    """Drop columns with little or no information value.
+
+    Changes to the dataframe are happening inplace.
+
+    Args:
+        df: pandas DataFrame
+    """
+    columns_to_drop = ["Unnamed: 0"]
+    for column in df.columns.values:
+        percentage_missing = df[column].isnull().sum() * 100 / len(df)
+        if percentage_missing >= 98:
+            columns_to_drop.append(column)
+            continue
+
+        unique_values = len(df[column].dropna().unique())
+        if unique_values == 1:
+            columns_to_drop.append(column)
+            continue
+
+        temp_df = df[column].value_counts().reset_index()
+        temp_df["percentage"] = temp_df["count"] / sum(temp_df["count"])
+        if temp_df["percentage"].max() >= 0.98:
+            columns_to_drop.append(column)
+
+    df.drop(columns_to_drop, axis=1, inplace=True)
+
+    print(f"Dropped {len(columns_to_drop)} columns:", columns_to_drop)
+
+
+if __name__ == "__main__":
+    df = pd.read_csv("example_raw_data.csv", sep=";", decimal=",")
+
+    drop_columns(df)
+    transform_column_types(df)
+    df = drop_rows(df)
